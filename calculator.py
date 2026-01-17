e979aa52c9931f388a7bed91e89432aae13609d8
Angelina Lozano <angielozarpi@gmail.com>
2026-01-16 22:19:16 -0500

    Add advanced mathematical operations

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math 
+
 # addition
 def add(n1, n2):
     return n1 + n2
@@ -12,4 +14,16 @@ def multiply(n1, n2):
 
 # division
 def divide(n1, n2):
+    if n2 == 0:
+        return "Cannot divide by zero"
     return n1 / n2
+
+# power
+def power(n1, n2):
+    return n1**n2
+
+# square root
+def square(n1, n2):
+    if n1 < 0:
+        return "Cannot take square root of negative number"
+    return math.sqrt(n1)
