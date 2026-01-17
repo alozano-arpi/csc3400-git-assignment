e53d0caf4c542ac94073e7491108d5f6dc885a14
Angelina Lozano <angielozarpi@gmail.com>
2026-01-16 22:13:06 -0500

    Add calculator module and gitignore

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# addition
+def add(n1, n2):
+    return n1 + n2
+
+# subtraction
+def sub(n1, n2):
+    return n1 - n2
+
+# multiplication
+def multiply(n1, n2):
+    return n1 * n2
+
+# division
+def divide(n1, n2):
+    return n1 / n2
